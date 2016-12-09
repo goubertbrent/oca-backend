a573aeca3cd4b709b861b3034bce1f874c44935e
Lucas Vanhalst <lucas@mobicage.com>
2016-12-09 14:47:02 +0100

    fix client error on tablets related to showing news

--- src/static/js/solutions/sln-news.js
+++ src/static/js/solutions/sln-news.js
@@ -15,58 +15,74 @@
  *
  * @@license_version:1.1@@
  */
-var newsInitialized = false;
-function initializeNews() {
-    sln.call({
-        url: '/common/news/all',
-        success: function (newsItems) {
-            // Do not show the news page if no news was found.
-            // This can happen when there are no articles yet in the user his language.
-            if (newsItems.length === 0) {
-                window.location.hash = '#';
-                if (!userClickedBeforeNewsLoaded) {
-                    $('#topmenu').find('a:first').click();
-                }
-            }
-            $.each(newsItems, function (i, n) {
-                n.dateFormatted = sln.format(new Date(n.datetime * 1000));
-            });
-            var html = $.tmpl(templates['news/news_item'], {
-                news: newsItems
-            });
-            $('#news-container').html(html);
-            $('.youtube-player').each(function () {
-                var $this = $(this);
-                var ytId = $this.attr('data-yt-id');
-                $this.click(function () {
-                    // open popup with this video
-                    var iframe = $('<iframe height="100%" frameborder="0" allowfullscreen>');
-                    iframe.attr('src', '//www.youtube.com/embed/' + ytId + '?autoplay=1&showinfo=1&fs=1');
-                    //$this.html(iframe);
-                    $('#video-yt-container').html(iframe);
-                    $("#video-popup").show()
-                        .modal('show')
-                        .on('hidden', function () {
-                            $('#video-yt-container').empty();
-                        });
-                });
-            });
+(function () {
+    'use strict';
+    var newsInitialized = false;
 
-            $('.coaching-sessions, .news').slick({
-                slidesToShow: 3,
-                slidesToScroll: 1,
-                dots: true,
-                infinite: false
-            });
-        }
-    });
-}
-function showNews() {
-    $("#topmenu li").removeClass("active");
-    $('.page').hide();
-    $('#newspage').show();
-    if (!newsInitialized) {
-        initializeNews();
-        newsInitialized = true;
+    init();
+
+    function init() {
+        ROUTES['news'] = router;
     }
-}
+
+    function router() {
+        showNews();
+    }
+
+    function initializeNews() {
+        sln.call({
+            url: '/common/news/all',
+            success: function (newsItems) {
+                // Do not show the news page if no news was found.
+                // This can happen when there are no articles yet in the user his language.
+                if (newsItems.length === 0) {
+                    window.location.hash = '#';
+                    if (!userClickedBeforeNewsLoaded) {
+                        $('#topmenu').find('a:first').click();
+                    }
+                }
+                $.each(newsItems, function (i, n) {
+                    n.dateFormatted = sln.format(new Date(n.datetime * 1000));
+                });
+                var html = $.tmpl(templates['news/news_item'], {
+                    news: newsItems
+                });
+                $('#news-container').html(html);
+                $('.youtube-player').each(function () {
+                    var $this = $(this);
+                    var ytId = $this.attr('data-yt-id');
+                    $this.click(function () {
+                        // open popup with this video
+                        var iframe = $('<iframe height="100%" frameborder="0" allowfullscreen>');
+                        iframe.attr('src', '//www.youtube.com/embed/' + ytId + '?autoplay=1&showinfo=1&fs=1');
+                        //$this.html(iframe);
+                        $('#video-yt-container').html(iframe);
+                        $("#video-popup").show()
+                            .modal('show')
+                            .on('hidden', function () {
+                                $('#video-yt-container').empty();
+                            });
+                    });
+                });
+
+                $('.coaching-sessions, .news').slick({
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                    dots: true,
+                    infinite: false
+                });
+            }
+        });
+    }
+
+    function showNews() {
+        $("#topmenu li").removeClass("active");
+        $('.page').hide();
+        $('#newspage').show();
+        if (!newsInitialized) {
+            initializeNews();
+            newsInitialized = true;
+        }
+    }
+
+})();
