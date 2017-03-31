6bfdd85993ad97d05f650401e0847ce8c47b408a
Donato Gomez <donato@greenitglobe.com>
2017-03-31 12:14:00 +0200

    Licenses

--- src-test/test/i18n/test_not_yet_translated.py
+++ src-test/test/i18n/test_not_yet_translated.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-# Copyright 2017 Mobicage NV
+# Copyright 2017 GIG Technology NV
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
