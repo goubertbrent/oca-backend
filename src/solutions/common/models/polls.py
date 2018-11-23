c841c2ec2f692f9519d8b32a12bf5061f25c7ce3
Abdelrahman Ghanem <abom.jdev@gmail.com>
2018-11-23 14:19:08 +0200

    Add poll register to prevent duplicate polls per user

--- src/solutions/common/models/polls.py
+++ src/solutions/common/models/polls.py
@@ -108,3 +108,13 @@ class Answer(NdbModel):
     @classmethod
     def create(cls, question_id, *values):
         return Answer(key=cls.create_key(question_id), values=values)
+
+
+class PollRegister(NdbModel):
+    created_on = ndb.DateTimeProperty(auto_now_add=True)
+    updated_on = ndb.DateTimeProperty(auto_now=True)
+
+    @classmethod
+    def create_key(cls, app_user, service_user, poll_id):
+        parent = Poll.create_key(service_user, poll_id)
+        return ndb.Key(cls, app_user.email(), parent=parent)
