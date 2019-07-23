e15eba11207a56f5377b8c39406380f8aa55ec6f
Bart Pede <bart@mobicage.com>
2019-07-23 10:14:03 +0200

    Making sure event image isn't cropped on the detail page Trying to fix syntax error in new dashboard

--- src/static/js/solutions/sln-new-dashboard.js
+++ src/static/js/solutions/sln-new-dashboard.js
@@ -16,15 +16,15 @@
         // Only load the iframe once user visits this page to speed up initial page load
         if (element.src) {
             element.contentWindow.postMessage({
-                type: 'oca.load_page',
-                paths: paths,
+                'type': 'oca.load_page',
+                'paths': paths
             }, DEBUG ? 'http://localhost:4300' : null);
         } else {
             element.src = baseUrl + paths[0];
             element.onload = function () {
                 element.contentWindow.postMessage({
-                    type: 'oca.set_language',
-                    language: LANGUAGE
+                    'type': 'oca.set_language',
+                    'language': LANGUAGE
                 }, DEBUG ? 'http://localhost:4300' : null);
             };
         }
